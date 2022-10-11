4bd8bfd354661c06e193311c77f7c24646b10814
tarunhimtreasure <113661453+tarunhimtreasure@users.noreply.github.com>
2022-10-11 18:33:35 +0530

    Day 3 _ final

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Contact Us
+ */
+?>
+<?php get_header(); ?>
+<?php get_header('hero'); ?>
+<main id="main">
+    <div class="container mt-4">
+        <h1 class="text-center" style="font-size:70px">
+            PAGE NOT FOUND 404 ERROR
+        </h1>
+    </div>
+</main>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title><?php the_title(); ?></title>
+    <title> <?php bloginfo('name'); ?> | <?php the_title(); ?> | <?php if(is_front_page()) { bloginfo('description'); } ?> </title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
